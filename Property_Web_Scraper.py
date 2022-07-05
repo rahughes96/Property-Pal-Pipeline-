919e5ae8a635c4e7a1a00728e9677ccd673bf258
rahughes96 <rahughes96@gmail.com>
2022-07-05 20:03:36 +0100

    Adds method to bypass cookies

--- Property_Web_Scraper.py
+++ Property_Web_Scraper.py
@@ -3,23 +3,56 @@ from selenium import webdriver
 from selenium.webdriver import Chrome
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import time
+
 
 class Scraper:
-    def __init__(self, url) -> None:
+    def __init__(self, url: str = 'https://www.propertypal.com') -> None:
         self.driver = Chrome(ChromeDriverManager().install())
         self.driver.get(url)
         
-    def accept_cookies(self, xpath):
+    def ivan_accept_cookies(self, xpath):
 
-        button = self.driver.find_element_by_xpath(xpath)
+        try:
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(By.XPATH, xpath))
+
+            button = self.driver.find_element(By.XPATH, xpath)
+            button.click()
+        except TimeoutException:
+            print("No Cookies Found")
+
+    def accept_cookies(self, xpath:str = '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'):
+        button = self.driver.find_element(By.XPATH, xpath)
         button.click()
+
     
     def search_word(self, xpath, text):
 
-        search_bar = self.driver.find_element_by_xpath(xpath)
-        search_bar.click()
-        search_bar.send_keys(text)
+        search = self.driver.find_element(By.XPATH, xpath)
+        self.driver.implicitly_wait(10)
+        ActionChains(self.driver).move_to_element(search).click(search).perform()
+        ActionChains(self.driver).send_keys(text).perform()
 
     def scroll_down(self):
         
         self.driver.execute_script("window.scrollTo(0, 540)") 
+    
+    def search_rent(self):
+        rent = self.driver.find_element_by_xpath('//*[@id="searchForm"]/div/div[2]/button[2]')
+        rent.click()
+
+    def search_sale(self):
+        rent = self.driver.find_element_by_xpath('//*[@id="searchForm"]/div/div[2]/button[1]')
+        rent.click()
+
+if __name__ == "__main__":
+    bot = Scraper()
+    bot.accept_cookies()
+    time.sleep(3)
+    bot.search_word('//*[@id="searchForm"]/div/div[1]', 'bt4')
+    time.sleep(3)
+    bot.search_rent()
